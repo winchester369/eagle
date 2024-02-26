3e147d99ad0bc88445c5a8fcd3082bac0b99c74a
BahAdoR <m4hc.coc19@gmail.com>
2024-02-26 18:05:29 +0330

    dont show username

--- web-ui.py
+++ web-ui.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 
 app = Flask("MONITOR_ROOM")
 
+
 def render_system_info_as_html_page():
     return render_template('dashboard.html')
 
@@ -13,14 +14,14 @@ def render_system_info_as_html_page():
 @app.route('/api/system_infos')
 def api_system_infos():
     servers = ger_servers_from_db()
-    servers_system_infos=[]
+    servers_system_infos = []
     for server in servers:
-        system_info=get_last_system_info_by_server_id(server_id=server['id'])
+        system_info = get_last_system_info_by_server_id(server_id=server['id'])
         print(system_info)
         # system_info=json.loads(system_info['json_data'])
         # system_info['id']=server['id']
-        system_info['code']=server['code']
-        system_info['username']=server['username']
+        system_info['code'] = server['code']
+        # system_info['username']=server['username']
         servers_system_infos.append(system_info)
     return json.dumps(servers_system_infos)
 
@@ -31,4 +32,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
