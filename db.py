049369046ed4b05686e2f473ea04679d3bb0f864
BahAdoR <m4hc.coc19@gmail.com>
2024-03-01 01:31:03 +0330

    add some charts

--- db.py
+++ db.py
@@ -1,13 +1,24 @@
 import json
 import sqlite3
 
+DB_FILE = 'db.sqlite3'
+
+
+def execute_query(query, parameters=None):
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        if parameters:
+            cursor.execute(query, parameters)
+        else:
+            cursor.execute(query)
+        conn.commit()
+        return cursor
+
 
 def ger_servers_from_db():
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM server')
-    server = cursor.fetchall()
-    conn.close()
+    query = 'SELECT * FROM server'
+    cursor = execute_query(query)
+
     servers = [{
         "id": server[0],
         "code": server[1],
@@ -15,42 +26,71 @@ def ger_servers_from_db():
         "password": server[3],
         "access_token": server[4],
         "base_url": server[5],
-    } for server in server]
+    } for server in cursor.fetchall()]
+
     return servers
 
 
 def set_access_token(server_id, access_token):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute('UPDATE server SET access_token = ? WHERE id = ?', (access_token, server_id))
-    conn.commit()
-    conn.close()
+    query = 'UPDATE server SET access_token = ? WHERE id = ?'
+    parameters = (access_token, server_id)
+    execute_query(query, parameters)
 
 
 def store_system_info(server_id, system_info):
-    # system_info = json.dumps(system_info)
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO system_info (server_id, json_data) VALUES (?, ?)', (server_id, system_info))
-    conn.commit()
-    conn.close()
+    query = 'INSERT INTO system_info (server_id, json_data) VALUES (?, ?)'
+    parameters = (server_id, system_info)
+    execute_query(query, parameters)
 
 
 def get_last_system_info_by_server_id(server_id):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM system_info WHERE server_id = ? ORDER BY id DESC LIMIT 1', (server_id,))
+    query = 'SELECT * FROM system_info WHERE server_id = ? ORDER BY id DESC LIMIT 1'
+    parameters = (server_id,)
+    cursor = execute_query(query, parameters)
+
     system_info = cursor.fetchone()
-    # print(system_info)
-    # system_info = system_info[0]
-    print(server_id)
-    json_data = json.loads(system_info[2])
-    # print(system_info[0])
-    conn.close()
-    return {
-        'id': system_info[0],
-        'server_id': system_info[1],
-        'json_data': json_data,
-        'created_at': system_info[3],
-        'updated_at': system_info[4],
-    }
+
+    if system_info:
+        json_data = json.loads(system_info[2])
+        return {
+            'id': system_info[0],
+            'server_id': system_info[1],
+            'json_data': json_data,
+            'created_at': system_info[3],
+            'updated_at': system_info[4],
+        }
+    else:
+        return None
+
+
+def get_last_system_info_chart_data(server):
+    query = 'SELECT created_at, json_data FROM system_info WHERE server_id = ? ORDER BY created_at LIMIT 10000'
+    parameters = (server['id'],)
+    cursor = execute_query(query, parameters)
+
+    chart_data_list = []
+
+    for record in cursor.fetchall():
+        try:
+            created_at = record[0]
+            json_data = json.loads(record[1])
+            # Assuming json_data structure includes online_users as a list
+            online_users = json_data.get('online_users_f_1_m', 0)
+            outgoing_bandwidth_speed = json_data.get('outgoing_bandwidth_speed', 0)
+            incoming_bandwidth_speed = json_data.get('incoming_bandwidth_speed', 0)
+            incoming_bandwidth = json_data.get('incoming_bandwidth', 0)
+            outgoing_bandwidth = json_data.get('outgoing_bandwidth', 0)
+            chart_data = {
+                'code': server['code'],
+                'created_at': created_at,
+                'online_user_count': online_users,
+                'total_speed': outgoing_bandwidth_speed+incoming_bandwidth_speed,
+                'total_used_traffic': incoming_bandwidth+outgoing_bandwidth,
+            }
+
+            chart_data_list.append(chart_data)
+        except Exception as e:
+            print(f'Error parsing JSON data: {e}')
+            continue
+
+    return chart_data_list
--- web-ui.py
+++ web-ui.py
@@ -1,8 +1,8 @@
 import json
 
-from db import ger_servers_from_db, get_last_system_info_by_server_id
+from db import ger_servers_from_db, get_last_system_info_by_server_id,get_last_system_info_chart_data
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask("MONITOR_ROOM")
 
@@ -14,16 +14,38 @@ def render_system_info_as_html_page():
 @app.route('/api/system_infos')
 def api_system_infos():
     servers = ger_servers_from_db()
-    servers_system_infos = []
-    for server in servers:
-        system_info = get_last_system_info_by_server_id(server_id=server['id'])
-        print(system_info)
-        # system_info=json.loads(system_info['json_data'])
-        # system_info['id']=server['id']
-        system_info['code'] = server['code']
-        # system_info['username']=server['username']
-        servers_system_infos.append(system_info)
-    return json.dumps(servers_system_infos)
+    try:
+        servers_system_infos = []
+        for server in servers:
+            system_info = get_last_system_info_by_server_id(server_id=server['id'])
+            # print(system_info)
+            # system_info=json.loads(system_info['json_data'])
+            system_info['id'] = server['id']
+            system_info['code'] = server['code']
+            # system_info['username']=server['username']
+            servers_system_infos.append(system_info)
+
+        return jsonify(servers_system_infos)
+        # return jsonify(servers_system_infos)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/api/system_charts')
+def api_system_charts():
+    try:
+        servers = ger_servers_from_db()
+        all_servers_chart_data = {}
+
+        for server in servers:
+            chart_data = get_last_system_info_chart_data(server=server)
+            # chart_data['code'] = server['code']
+            all_servers_chart_data[server['id']] = chart_data
+
+        return jsonify(all_servers_chart_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/')
